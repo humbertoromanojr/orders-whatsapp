4ab2a4537bc371ac8ff0dfb4963392287d2b9f3e
humbertojr <astronomi@gmail.com>
2024-02-08 20:41:17 -0300

    Updated component header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
-import { View, Image, Text } from "react-native"
+import { View, Image, Text, TouchableOpacity } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import colors from "tailwindcss/colors"
 
 type HeaderProps = {
   title: string
+  cartQuantityItems?: number
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, cartQuantityItems = 0 }: HeaderProps) {
   return (
     <View
       style={{
@@ -12,15 +15,45 @@ export function Header({ title }: HeaderProps) {
         borderBottomWidth: 1,
         borderColor: "#555",
         flexDirection: "row",
+        justifyContent: "space-between",
       }}
     >
-      <View style={{ flex: 1, paddingBottom: 5 }}>
+      <View style={{ flex: 1 }}>
         <Image
           style={{ width: "45%", height: 20 }}
           source={require("@/assets/logo.png")}
         />
+        <Text style={{ color: "#eee", paddingVertical: 10 }}>{title}</Text>
       </View>
-      <Text style={{ color: "#eee" }}>{title}</Text>
+
+      {cartQuantityItems > 0 && (
+        <TouchableOpacity style={{ position: "relative" }} activeOpacity={0.7}>
+          <View
+            style={{
+              backgroundColor: "#0f0",
+              width: 16,
+              height: 16,
+              borderRadius: 8,
+              justifyContent: "center",
+              alignItems: "center",
+              position: "absolute",
+              marginLeft: -7,
+              zIndex: 9,
+            }}
+          >
+            <Text style={{ color: "#222", fontWeight: "bold", fontSize: 10 }}>
+              {cartQuantityItems}
+            </Text>
+          </View>
+
+          <Feather
+            name="shopping-bag"
+            color={colors.white}
+            size={24}
+            style={{ paddingTop: 9 }}
+          />
+        </TouchableOpacity>
+      )}
     </View>
   )
 }
