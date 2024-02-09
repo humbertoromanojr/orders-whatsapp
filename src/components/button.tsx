f4088714d996d31d74f04c39a9c68fac97ef64fc
humbertojr <astronomi@gmail.com>
2024-02-09 20:08:19 -0300

    Finished the custom component button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,47 @@
-import React from "react"
-import { View } from "react-native"
+import React, { ReactNode } from "react"
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native"
 
-export function Button() {
-  return <View></View>
+type ButtonProps = TouchableOpacityProps & {
+  children: ReactNode
 }
+
+type ButtonTextProps = {
+  children: ReactNode
+}
+type ButtonIconProps = {
+  children: ReactNode
+}
+
+function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      {...rest}
+      style={{
+        backgroundColor: "#17233d",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        borderRadius: 7,
+      }}
+    >
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+function ButtonText({ children }: ButtonTextProps) {
+  return (
+    <Text style={{ color: "#fff", fontSize: 14, fontWeight: "bold" }}>
+      {children}
+    </Text>
+  )
+}
+
+function ButtonIcon({ children }: ButtonIconProps) {
+  return children
+}
+
+Button.text = ButtonText
+Button.Icon = ButtonIcon
+
+export { Button }
