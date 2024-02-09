654a4ca8cbf781221881edfb72e432de32eea537
humbertojr <astronomi@gmail.com>
2024-02-09 19:33:07 -0300

    Applying currency format

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,11 +1,26 @@
 import React from "react"
-import { View } from "react-native"
+import { View, Image, Text } from "react-native"
 import { useLocalSearchParams } from "expo-router"
+
+import { PRODUCTS } from "@/utils/data/products"
+import { formatCurrency } from "@/utils/functions/format-currency"
 
 export default function Product() {
   const { id } = useLocalSearchParams()
 
-  console.log("== [id] > => ", id)
+  const product = PRODUCTS.filter((item) => item.id === id)[0]
 
-  return <View style={{ flex: 1 }}></View>
+  return (
+    <View style={{ flex: 1 }}>
+      <Image
+        source={product.cover}
+        resizeMode="cover"
+        style={{ width: "100%", height: "30%" }}
+      />
+
+      <View style={{ flex: 1, padding: 5, marginTop: 8 }}>
+        <Text style={{ color: "#fff" }}>{formatCurrency(product.price)}</Text>
+      </View>
+    </View>
+  )
 }
