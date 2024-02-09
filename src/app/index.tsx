ab27f2d188ad2498ed3f16ae60b7afd900c79763
humbertojr <astronomi@gmail.com>
2024-02-09 09:29:25 -0300

    useState to  select category

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,37 @@
-import { View } from "react-native"
+import { useState } from "react"
+import { View, FlatList } from "react-native"
 
 import { Header } from "@/components/header"
+import { CategoryButton } from "@/components/category-button"
+
+import { CATEGORIES } from "@/utils/data/products"
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelected(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
     <View>
-      <Header title="Faça o seu pedido!" />
+      <Header title="Faça o seu pedido" cartQuantityItems={1} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)}
+          />
+        )}
+        style={{ marginTop: 10 }}
+        contentContainerStyle={{ gap: 12 }}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
     </View>
   )
 }
