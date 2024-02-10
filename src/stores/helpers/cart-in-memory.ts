3037e9a62beea5600fb3a6e29eb312d67b2d9c08
humbertojr <astronomi@gmail.com>
2024-02-10 12:04:47 -0300

    Verify if exist same id

--- src/stores/helpers/cart-in-memory.ts
+++ src/stores/helpers/cart-in-memory.ts
@@ -1,4 +1,10 @@
 import { ProductProps } from "@/utils/data/products"
 import { ProductCartProps } from "../cart-store"
 
-export function add(products: ProductCartProps[], newProduct: ProductProps) {}
+export function add(products: ProductCartProps[], newProduct: ProductProps) {
+  const existingProduct = products.find(({ id }) => newProduct.id === id)
+
+  if (existingProduct) {
+    return products.map((product) => product.id === existingProduct.id)
+  }
+}
