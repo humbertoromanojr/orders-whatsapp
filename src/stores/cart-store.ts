fe2994d6efd0494c03edf314a3ff2d21cb465f97
humbertojr <astronomi@gmail.com>
2024-02-10 11:56:14 -0300

    Created cart store

---
+++ src/stores/cart-store.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand"
+
+import { ProductProps } from "@/utils/data/products"
+
+export type ProductCartProps = ProductProps & {
+  quantity: number
+}
+
+type StateProps = {
+  products: ProductCartProps[]
+  add: (product: ProductProps) => void
+}
+
+export const useCartStore = create<StateProps>((set) => ({
+  products: [],
+
+  add: () => {},
+}))
