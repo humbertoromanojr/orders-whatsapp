ad22fe063cf7bcb2253f6f6ed0f79afff08d43b3
humbertojr <astronomi@gmail.com>
2024-02-11 13:25:07 -0300

    Configuring input component

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { View, Text, ScrollView } from "react-native"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 import { useCartStore } from "@/stores/cart-store"
 
@@ -7,6 +8,7 @@ import { formatCurrency } from "@/utils/functions/format-currency"
 
 import { Header } from "@/components/header"
 import { Product } from "@/components/product"
+import { Input } from "@/components/input"
 
 export default function Cart() {
   const CartStore = useCartStore()
@@ -22,43 +24,54 @@ export default function Cart() {
     <View style={{ flex: 1, paddingTop: 7 }}>
       <Header title="Seu carrinho" />
 
-      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-        {CartStore.products.length > 0 ? (
-          <View style={{ flex: 1, padding: 5 }}>
-            {CartStore.products.map((product) => (
-              <Product key={product.id} data={product} />
-            ))}
-          </View>
-        ) : (
-          <Text
+      <KeyboardAwareScrollView>
+        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          {CartStore.products.length > 0 ? (
+            <View
+              style={{
+                flex: 1,
+                padding: 5,
+                borderBottomWidth: 1,
+                borderBottomColor: "#555",
+              }}
+            >
+              {CartStore.products.map((product) => (
+                <Product key={product.id} data={product} />
+              ))}
+            </View>
+          ) : (
+            <Text
+              style={{
+                flex: 1,
+                textAlignVertical: "center",
+                marginTop: 16,
+                textAlign: "center",
+                fontWeight: "bold",
+                color: "#fff",
+              }}
+            >
+              Seu carrinho esta vazio!
+            </Text>
+          )}
+
+          <View
             style={{
-              flex: 1,
-              textAlignVertical: "center",
-              marginTop: 16,
-              textAlign: "center",
-              fontWeight: "bold",
-              color: "#fff",
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 20,
+              marginTop: 12,
             }}
           >
-            Seu carrinho esta vazio!
-          </Text>
-        )}
+            <Text style={{ color: "#fff" }}>Total:</Text>
 
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginBottom: 20,
-            marginTop: 12,
-          }}
-        >
-          <Text style={{ color: "#fff" }}>Total:</Text>
+            <Text style={{ color: "#0f0", marginLeft: 10, fontWeight: "bold" }}>
+              {valueTotal}
+            </Text>
+          </View>
 
-          <Text style={{ color: "#0f0", marginLeft: 10, fontWeight: "bold" }}>
-            {valueTotal}
-          </Text>
-        </View>
-      </ScrollView>
+          <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..." />
+        </ScrollView>
+      </KeyboardAwareScrollView>
     </View>
   )
 }
